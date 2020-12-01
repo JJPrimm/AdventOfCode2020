6dd3ca8646fc97e68f8d7d7613ca57c400ee412b
Joshua Primm <joshua.primm@moserit.com>
2020-11-30 20:49:48 -0500

    Initial Commit

---
+++ Day1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode
+{
+    public static class Day1
+    {
+        public static void Problem1()
+        {
+
+        }
+        public static void Problem2()
+        {
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AdventOfCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Advent of Code 2020");
+            Day1.Problem1();
+            Day1.Problem2();
+
+            Console.ReadKey();
+        }
+    }
+}
