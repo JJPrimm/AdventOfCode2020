64fdef2c023772f5682222c0a28ba54f7a83165a
Joshua Primm <joshua.primm@moserit.com>
2020-12-03 00:20:37 -0500

    Day 3 complete

---
+++ Day3.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode
+{
+    public class Day3
+    {
+        public static void Problem1()
+        {
+            var slope = Utilities.ReadStringArray(3).ToArray();
+            
+            Console.WriteLine(TreeCount(slope, 3, 1));
+        }
+
+        public static void Problem2()
+        {
+            var slope = Utilities.ReadStringArray(3).ToArray();
+            long trees = TreeCount(slope, 1, 1);
+            trees *= TreeCount(slope, 3, 1);
+            trees *= TreeCount(slope, 5, 1);
+            trees *= TreeCount(slope, 7, 1);
+            trees *= TreeCount(slope, 1, 2);
+
+            Console.WriteLine(trees);
+        }
+
+        private static int TreeCount(string[] slope, int r, int d)
+        {
+            int col = 0;
+            int trees = 0;
+            int row = 0;
+            int width = slope[0].Length;
+            while (row < slope.Count())
+            {
+                if (slope[row][col] == '#')
+                {
+                    trees++;
+                }
+                row += d;
+                col = (col + r) % width;
+            }
+            return trees;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,7 +10,9 @@ namespace AdventOfCode
             //Day1.Problem1();
             //Day1.Problem2();
             //Day2.Problem1();
-            Day2.Problem2();
+            //Day2.Problem2();
+            //Day3.Problem1();
+            Day3.Problem2();
 
             Console.ReadKey();
         }
