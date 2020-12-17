19167092f3ac326ccf75beae1fd7156d5a729e4a
Joshua Primm <joshua.primm@moserit.com>
2020-12-16 20:12:14 -0500

    Reversed the order of the Days in the Main method, so I don't have to scroll to get to the latest. God, I'm lazy.

---
+++ Day17.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace AdventOfCode
+{
+    public static class Day17
+    {
+        public static void Problem1()
+        {
+
+        }
+
+        public static void Problem2()
+        {
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,39 +6,46 @@ namespace AdventOfCode
     {
         static void Main(string[] args)
         {
+
+            // Some might ask, why not automate this? Just accept the day and problem number as user input.
+            // The answer is because I am a terrible programmer. I know I'm going to have to stop and restart each problem repeatedly.
+            // So in the end, it's probably more efficient for me to hardcode the day and problem I'm working on like this.
+
             Console.WriteLine("Advent of Code 2020");
-            //Day01.Problem1();
-            //Day01.Problem2();
-            //Day02.Problem1();
-            //Day02.Problem2();
-            //Day03.Problem1();
-            //Day03.Problem2();
-            //Day04.Problem1();
-            //Day04.Problem2();
-            //Day05.Problem1();
-            //Day05.Problem2();
-            //Day06.Problem1();
-            //Day06.Problem2();
-            //Day07.Problem1();
-            //Day07.Problem2();
-            //Day08.Problem1();
-            //Day08.Problem2();
-            //Day09.Problem1();
-            //Day09.Problem2();
-            //Day10.Problem1();
-            //Day10.Problem2();
-            //Day11.Problem1();
-            //Day11.Problem2();
-            //Day12.Problem1();
-            //Day12.Problem2();
-            //Day13.Problem1();
-            //Day13.Problem2();
-            //Day14.Problem1();
-            //Day14.Problem2();
-            //Day15.Problem1();
-            //Day15.Problem2();
+            //Day17.Problem2();
+            Day17.Problem1();
+            //Day16.Problem2();
             //Day16.Problem1();
-            Day16.Problem2();
+            //Day15.Problem2();
+            //Day15.Problem1();
+            //Day14.Problem2();
+            //Day14.Problem1();
+            //Day13.Problem2();
+            //Day13.Problem1();
+            //Day12.Problem2();
+            //Day12.Problem1();
+            //Day11.Problem2();
+            //Day11.Problem1();
+            //Day10.Problem2();
+            //Day10.Problem1();
+            //Day09.Problem2();
+            //Day09.Problem1();
+            //Day08.Problem2();
+            //Day08.Problem1();
+            //Day07.Problem2();
+            //Day07.Problem1();
+            //Day06.Problem2();
+            //Day06.Problem1();
+            //Day05.Problem2();
+            //Day05.Problem1();
+            //Day04.Problem2();
+            //Day04.Problem1();
+            //Day03.Problem2();
+            //Day03.Problem1();
+            //Day02.Problem2();
+            //Day02.Problem1();
+            //Day01.Problem2();
+            //Day01.Problem1();
 
             Console.ReadKey();
         }
